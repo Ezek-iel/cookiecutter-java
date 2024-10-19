0429799ac90d6663b1b529f0051de9c22831cbf4
Ezekiel <ezekielakinfenwa98@gmail.com>
2024-10-19 12:37:54 +0100

    Little updates

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,5 @@
+import os, sys
+
+current_directory = os.getcwd()
+os.system(f'cd {current_directory} && code . --profile Java')
+sys.exit(0)
---
+++ hooks/pre_gen_project.py
@@ -0,0 +1,5 @@
+import colorama
+from colorama import Fore
+
+colorama.init
+print(Fore.GREEN + "Creating a cookiecutter template")
